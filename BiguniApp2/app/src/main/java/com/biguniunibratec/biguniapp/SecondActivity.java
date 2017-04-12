7bfd8a8b46c2c5a05c6e13f5cef4664d52478d0c
TOSHIBA\JOHN <johnoestoy@gmail.com>
2017-04-12 15:24:16 -0300

    Criacao de tela 1 e 2, edicao de cores

---
+++ BiguniApp2/app/src/main/java/com/biguniunibratec/biguniapp/SecondActivity.java
@@ -0,0 +1,10 @@
+package com.biguniunibratec.biguniapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JOHN on 12/04/2017.
+ */
+
+public class SecondActivity extends AppCompatActivity {
+}
