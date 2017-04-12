bc04c833a9792f479fa89ae106b8da0548b0e9ac
LAPTOP-F2B1CM1M\Rayssa&Thiago <rayssa.nog.s@hotmail.com>
2017-04-12 20:14:11 -0300

    gradle

---
+++ BiguniApp2/app/src/main/java/com/biguniunibratec/biguniapp/NewAccountActivity.java
@@ -0,0 +1,10 @@
+package com.biguniunibratec.biguniapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rayssa&Thiago on 12/04/2017.
+ */
+
+public class NewAccountActivity extends AppCompatActivity {
+}
