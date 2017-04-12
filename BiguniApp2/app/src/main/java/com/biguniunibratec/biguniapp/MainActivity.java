698a340ffa6327241c25a6147c221464ce272c71
LAPTOP-F2B1CM1M\Rayssa&Thiago <rayssa.nog.s@hotmail.com>
2017-04-12 19:28:59 -0300

    botao

--- BiguniApp2/app/src/main/java/com/biguniunibratec/biguniapp/MainActivity.java
+++ BiguniApp2/app/src/main/java/com/biguniunibratec/biguniapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.biguniunibratec.biguniapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context c = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonhome= (Button) findViewById(R.id.buttonhome);
+        buttonhome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent =new Intent(c, |SecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+
     }
 }
