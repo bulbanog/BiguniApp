4fbaa2ce5906f08589ab6daa2a6d0aecd7351c0c
LAPTOP-F2B1CM1M\Rayssa&Thiago <rayssa.nog.s@hotmail.com>
2017-04-19 19:53:40 -0300

    Ligando Layouts a Classes

---
+++ BiguniApp2/app/src/main/java/CorridaAdapter.java
@@ -0,0 +1,12 @@
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by Rayssa&Thiago on 19/04/2017.
+ */
+
+public class CorridaAdapter extends  RecyclerView.ViewHolder{
+    public CorridaAdapter(View itemView) {
+        super(itemView);
+    }
+}
--- BiguniApp2/app/src/main/java/com/biguniunibratec/biguniapp/NewAccountActivity3.java
+++ BiguniApp2/app/src/main/java/com/biguniunibratec/biguniapp/NewAccountActivity3.java
@@ -1,24 +1,20 @@
 package com.biguniunibratec.biguniapp;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
 /**
- * Created by Rayssa&Thiago on 12/04/2017.
+ * Created by Rayssa&Thiago on 19/04/2017.
  */
 
-public class NewAccountActivity extends AppCompatActivity {
+public class NewAccountActivity3 extends AppCompatActivity {
     private Context c = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_newaccount);
+        setContentView(R.layout.activity_newaccount3);
 
     }
 }
-
